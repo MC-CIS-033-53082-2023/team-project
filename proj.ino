80bead8afbf198e91c61ef3f50847090c66610ed
Yizhe Wang <will.yizhewang@gmail.com>
2023-07-06 00:04:16 -0700

    init setup

---
+++ proj.ino
@@ -0,0 +1,17 @@
+// Description: Short description of the project
+
+// include libraries needed for lcd, servo motor and other sensors
+
+// constants and global variables
+
+// setup function
+void setup()
+{
+    // code setup
+}
+
+// loop function
+void loop()
+{
+    // code loop
+}
